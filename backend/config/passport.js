b30a98153db34ade706dec860caac85149017ecd
jenil1236 <jenilp2146@gmail.com>
2025-11-03 00:10:09 +0530

    final-3

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -31,7 +31,7 @@ export default function setupPassport(passport) {
       {
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: "http://localhost:5000/api/auth/google/callback",
+        callbackURL: process.env.GOOGLE_CALLBACK_URL,
       },
       async (accessToken, refreshToken, profile, done) => {
         try {
